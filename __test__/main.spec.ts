a9396fbfe6f364bb0566f4dbe3873435127e76c3
Romén Rodríguez-Gil <romen@romenrg.com>
2019-09-07 13:06:13 +0100

    Second test passing: order is taken into account

--- __test__/main.spec.ts
+++ __test__/main.spec.ts
@@ -11,4 +11,9 @@ describe('Test', () => {
     const dojo = new Dojo(testArray);
     expect(dojo.calcOrderedRepetitionsList()).toEqual([1]);
   });
+  it('Should return two elements. Order following the first duplicate', () => {
+    const testArray: Array<number> = [1, 2, 2, 1];
+    const dojo = new Dojo(testArray);
+    expect(dojo.calcOrderedRepetitionsList()).toEqual([2, 1]);
+  });
 });
--- src/dojo.ts
+++ src/dojo.ts
@@ -3,7 +3,7 @@
  * Should return an array with the numbers that are repeated, sorted by first repeated element
  */
 export class Dojo {
-  public static COMMANDS = 'Coding Dojo';
+  public static MAX = 999;
 
   public elementsList: Array<number>;
 
@@ -12,9 +12,41 @@ export class Dojo {
   }
 
   public calcOrderedRepetitionsList(): Array<number> {
-    if (this.elementsList.length === 0)
+    if (this.elementsList.length === 0) {
       return [];
-    else
-      return [1];
+    } else {
+      let result: Array<number> = [];
+      let distancesArray: Array<number> = new Array<number>(this.elementsList.length);
+      for (let i = 0; i < this.elementsList.length; i++) {
+        for (let j = i + 1; j < this.elementsList.length; j++) {
+          if (this.elementsList[i] === this.elementsList[j]) {
+            distancesArray[i] = j - i; //TODO: Break for third occurrence
+          } else if ((j === this.elementsList.length - 1) && (distancesArray[i] === undefined)) {
+            distancesArray[i] = Dojo.MAX;
+          }
+        }
+      }
+      distancesArray[distancesArray.length - 1] = Dojo.MAX;
+      for (let i = 0; i < this.elementsList.length; i++) {
+        const minIndex = this.findMinIndex(distancesArray);
+        if (minIndex !== Dojo.MAX && distancesArray[minIndex] !== Dojo.MAX) {
+          result.push(this.elementsList[minIndex]);
+          distancesArray[minIndex] = Dojo.MAX;
+        }
+      }
+      return result;
+    }
+  }
+
+  private findMinIndex(distancesArray: Array<number>) : number {
+    let min = Dojo.MAX;
+    let index = Dojo.MAX;
+    for (let i = 0; i < this.elementsList.length; i++) {
+      if (distancesArray[i] < min) {
+        min = distancesArray[i];
+        index = i;
+      }
+    }
+    return index;
   }
 }
